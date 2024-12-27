edbb0330b216bc571a004d4a8b9c86f42b4892c2
Rohit Nair <rohitnair099@outlook.com>
2024-12-27 15:12:17 +0000

    tailwind and board setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { io } from "socket.io-client";
+import { Chess } from "chess.js";
 const socket = io.connect("http://localhost:3000");
 
 const App = () => {
@@ -8,8 +9,8 @@ const App = () => {
     console.log("Hello there received");
   });
   return (
-    <div>
-      <h1>Chess</h1>
+    <div className="w-full h-screen bg-zinc-900 flex items-center justify-center">
+      <div className="chessboard w-96 h-96 bg-red-400 "></div>
     </div>
   );
 };
